97a632c9c0618b89512e5efae8a3072acd60618d
Zoyter <oleglyash@mail.ru>
2019-04-14 19:30:43 +0300

    Начал пересмотр кода для соответствия "логике работы" от заказчика

---
+++ start2.py
@@ -0,0 +1,24 @@
+#-*- coding:utf-8 -*-
+# Импорт модулей
+import arcade
+import os
+import tkinter as tk
+
+# Включение и отключение вывода отладочных сообщений
+DEBUG = True
+
+# Основной класс окна, в котором все рисуется
+class TApp(arcade.Window):
+    """ Основной класс приложения """
+    def __init__(self, fs=False):
+        """ Конструктор """
+        pass
+
+    def setAll(self):
+        pass
+
+
+def main():
+    """ Основная функция программы """
+if __name__ == "__main__":
+    main()
